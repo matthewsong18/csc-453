82c00447919f238f77a37e0af4db4a7dbc37ac6c
Matthew Song <matthewsong18@gmail.com>
2025-02-27 22:39:46 -0700

    feat: add g2 tokens

--- assignments/assignment-2-2/token_service.c
+++ assignments/assignment-2-2/token_service.c
@@ -8,32 +8,68 @@ TokenI currentToken;
 
 // Call scanner
 TokenI getNextToken(void) {
-  int tok;
+  int tok = get_token();
   TokenI token;
-  if ((tok = get_token()) == EOF) {
-    token.type = TOKEN_EOF;
-  }
-
-  if (tok == ID)
-    token.type = TOKEN_ID;
-  else if (tok == kwINT)
-    token.type = TOKEN_KWINT;
-  else if (tok == LPAREN)
-    token.type = TOKEN_LPAREN;
-  else if (tok == RPAREN)
-    token.type = TOKEN_RPAREN;
-  else if (tok == LBRACE)
-    token.type = TOKEN_LBRACE;
-  else if (tok == RBRACE)
-    token.type = TOKEN_RBRACE;
-  else if (tok == SEMI)
-    token.type = TOKEN_SEMI;
-  else if (tok == UNDEF)
-    token.type = TOKEN_UNDEF;
-  else if (tok == COMMA)
-    token.type = TOKEN_COMMA;
-
   token.line = currentLine;
+
+  switch (tok) {
+  case EOF:
+    token.type = TOKEN_EOF;
+    break;
+  case ID:
+    token.type = TOKEN_ID;
+    break;
+  case kwINT:
+    token.type = TOKEN_KWINT;
+    break;
+  case LPAREN:
+    token.type = TOKEN_LPAREN;
+    break;
+  case RPAREN:
+    token.type = TOKEN_RPAREN;
+    break;
+  case LBRACE:
+    token.type = TOKEN_LBRACE;
+    break;
+  case RBRACE:
+    token.type = TOKEN_RBRACE;
+    break;
+  case SEMI:
+    token.type = TOKEN_SEMI;
+    break;
+  case UNDEF:
+    token.type = TOKEN_UNDEF;
+    break;
+  case COMMA:
+    token.type = TOKEN_COMMA;
+    break;
+  case kwWHILE:
+    token.type = TOKEN_KWWHILE;
+  case kwIF:
+    token.type = TOKEN_KWIF;
+  case kwRETURN:
+    token.type = TOKEN_KWRETURN;
+  case opASSG:
+    token.type = TOKEN_OPASSG;
+  case INTCON:
+    token.type = TOKEN_INTCON;
+  case opEQ:
+    token.type = TOKEN_OPEQ;
+  case opNE:
+    token.type = TOKEN_OPNE;
+  case opLE:
+    token.type = TOKEN_OPLE;
+  case opLT:
+    token.type = TOKEN_OPLT;
+  case opGE:
+    token.type = TOKEN_OPGE;
+  case opGT:
+    token.type = TOKEN_OPGT;
+
+  default:
+    token.type = TOKEN_UNDEF;
+    break;
+  }
   return token;
 }
 
