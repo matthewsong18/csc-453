8cff869fe7b7c0e82bf5cf136133cabed5472102
Matthew Song <matthewsong18@gmail.com>
2025-04-23 19:59:30 -0700

    feat: parse function definition

--- assignments/assignment-4-1/src/features/parser/simple-parse.c
+++ assignments/assignment-4-1/src/features/parser/simple-parse.c
@@ -1,9 +1,34 @@
-
 #include "simple-parse.h"
 #include <stdlib.h>
 
+// Forward Declarations
+parse_data *parse_decl(parse_data *data);
+parse_data *parse_decl_prime(parse_data *data);
+parse_data *parse_var_decl_list_tail(parse_data *data);
+parse_data *parse_func_defn(parse_data *data);
+parse_data *parse_opt_formals(parse_data *data);
+parse_data *parse_formals_list(parse_data *data);
+parse_data *parse_formals_list_tail(parse_data *data);
+parse_data *parse_opt_var_decls(parse_data *data);
+parse_data *parse_stmt_list(parse_data *data);
+parse_data *parse_stmt(parse_data *data);
+parse_data *parse_if_stmt(parse_data *data);
+parse_data *parse_else_part(parse_data *data);
+parse_data *parse_while_stmt(parse_data *data);
+parse_data *parse_stmt_after_id(parse_data *data);
+parse_data *parse_opt_actuals(parse_data *data);
+parse_data *parse_arith_exp(parse_data *data);
+parse_data *parse_term(parse_data *data);
+parse_data *parse_factor(parse_data *data);
+parse_data *parse_primary_exp(parse_data *data);
+parse_data *parse_arith_exp_prime(parse_data *data);
+parse_data *parse_term_prime(parse_data *data);
+static parse_data *parse_type(parse_data *data);
+
+
 static parse_data *create_parse_data(void) {
   parse_data *data = malloc(sizeof(parse_data));
+  if (!data) exit(EXIT_FAILURE);
   data->current_token = next_token();
   data->exit_status = 0;
   return data;
@@ -13,24 +38,25 @@ void update_data_to_next_token(parse_data *data) {
   data->current_token = next_token();
 }
 
-static int does_not_match(token_type expected_token,
-                          token_data actual_token_data) {
+static int does_not_match(const token_type expected_token,
+                          const token_data actual_token_data) {
   if (expected_token != actual_token_data.type) {
     return 1;
   }
-
   return 0;
 }
 
-static int in_first_set(token_type expected_token, parse_data *data) {
-  if (does_not_match(expected_token, data->current_token)) {
-    return 0;
-  }
-
-  return 1;
+static int in_first_set(const token_type expected_token,
+                        const parse_data *data) {
+  if (!data) return 0;
+  return !does_not_match(expected_token, data->current_token);
 }
 
-int bad_exit(const parse_data *data) { return data->exit_status == 1; }
+int bad_exit(const parse_data *data) {
+    if (!data) return 1;
+    return data->exit_status == 1;
+}
+
 
 static parse_data *parse_type(parse_data *data) {
   if (does_not_match(TOKEN_KWINT, data->current_token)) {
@@ -38,34 +64,23 @@ static parse_data *parse_type(parse_data *data) {
     return data;
   }
   update_data_to_next_token(data);
-
   return data;
 }
 
-parse_data *parse_var_decl_list_tail(parse_data *data) {
-  // Loop as long as the next token is a comma
-  while (!bad_exit(data) && in_first_set(TOKEN_COMMA, data)) {
-    update_data_to_next_token(data); // Consume COMMA
 
-    // After a comma, we expect a type (like kwINT)
+parse_data *parse_var_decl_list_tail(parse_data *data) {
+  while (!bad_exit(data) && in_first_set(TOKEN_COMMA, data)) {
+    update_data_to_next_token(data);
     data = parse_type(data);
     if (bad_exit(data)) {
-      // Error detected within parse_type (e.g., missing 'int')
       return data;
     }
-
-    // After the type, we expect an ID
     if (does_not_match(TOKEN_ID, data->current_token)) {
-      // Error: Missing ID after 'COMMA type' sequence
-      // Handle the "int x, int ," case
       data->exit_status = 1;
       return data;
     }
-    update_data_to_next_token(data); // Consume ID
+    update_data_to_next_token(data);
   }
-
-  // If the loop finishes (no more commas or an error occurred earlier),
-  // return the current data state. This handles the epsilon case naturally.
   return data;
 }
 
@@ -74,46 +89,315 @@ parse_data *parse_decl_prime(parse_data *data) {
   if (bad_exit(data)) {
     return data;
   }
-
   if (does_not_match(TOKEN_SEMI, data->current_token)) {
     data->exit_status = 1;
     return data;
   }
   update_data_to_next_token(data);
-
-  return data;
-}
-parse_data *parse_decl(parse_data *data) {
-  data = parse_type(data);
-  if (bad_exit(data)) {
-    return data;
-  }
-
-  if (does_not_match(TOKEN_ID, data->current_token)) {
-    data->exit_status = 1;
-    return data;
-  }
-  update_data_to_next_token(data);
-
-  data = parse_decl_prime(data);
-  if (bad_exit(data)) {
-    return data;
-  }
-
   return data;
 }
 
-parse_data *parse_decl_list(parse_data *data) {
-  // Check FIRST set for 'decl', which starts with 'type' (TOKEN_KWINT)
+
+parse_data *parse_opt_var_decls(parse_data *data) {
   while (!bad_exit(data) && in_first_set(TOKEN_KWINT, data)) {
     data = parse_decl(data);
   }
   return data;
 }
 
-int parse_prog() {
-  parse_data *data = create_parse_data();
-  return parse_decl_list(data)->exit_status;
+
+parse_data *parse_formals_list_tail(parse_data *data) {
+  while (!bad_exit(data) && in_first_set(TOKEN_COMMA, data)) {
+    update_data_to_next_token(data);
+    data = parse_type(data);
+    if (bad_exit(data)) {
+      data->exit_status = 1;
+      return data;
+    }
+    if (does_not_match(TOKEN_ID, data->current_token)) {
+      data->exit_status = 1;
+      return data;
+    }
+    update_data_to_next_token(data);
+  }
+  return data;
 }
 
-int parse(void) { return parse_prog(); }
+parse_data *parse_formals_list(parse_data *data) {
+  data = parse_type(data);
+  if (bad_exit(data)) {
+    return data;
+  }
+  if (does_not_match(TOKEN_ID, data->current_token)) {
+    data->exit_status = 1;
+    return data;
+  }
+  update_data_to_next_token(data);
+  data = parse_formals_list_tail(data);
+  return data;
+}
+
+parse_data *parse_opt_formals(parse_data *data) {
+  if (in_first_set(TOKEN_KWINT, data)) {
+    data = parse_formals_list(data);
+  }
+  return data;
+}
+
+
+parse_data *parse_func_defn(parse_data *data) {
+  if (does_not_match(TOKEN_LPAREN, data->current_token)) {
+    data->exit_status = 1;
+    return data;
+  }
+  update_data_to_next_token(data);
+
+  data = parse_opt_formals(data);
+  if (bad_exit(data)) {
+    return data;
+  }
+
+  if (does_not_match(TOKEN_RPAREN, data->current_token)) {
+    data->exit_status = 1;
+    return data;
+  }
+  update_data_to_next_token(data);
+
+  if (in_first_set(TOKEN_LBRACE, data)) {
+    update_data_to_next_token(data);
+    data = parse_opt_var_decls(data);
+    if (bad_exit(data)) {
+      return data;
+    }
+    data = parse_stmt_list(data);
+    if (bad_exit(data)) {
+      return data;
+    }
+    if (does_not_match(TOKEN_RBRACE, data->current_token)) {
+      data->exit_status = 1;
+      return data;
+    }
+    update_data_to_next_token(data);
+  } else if (in_first_set(TOKEN_SEMI, data)) {
+    update_data_to_next_token(data);
+  } else {
+    data->exit_status = 1;
+    return data;
+  }
+  return data;
+}
+
+
+parse_data *parse_decl(parse_data *data) {
+  data = parse_type(data);
+  if (bad_exit(data)) {
+    return data;
+  }
+  if (does_not_match(TOKEN_ID, data->current_token)) {
+    data->exit_status = 1;
+    return data;
+  }
+  update_data_to_next_token(data);
+
+  if (in_first_set(TOKEN_LPAREN, data)) {
+    data = parse_func_defn(data);
+  } else {
+    data = parse_decl_prime(data);
+  }
+  return data;
+}
+
+parse_data *parse_decl_list(parse_data *data) {
+  while (!bad_exit(data) && in_first_set(TOKEN_KWINT, data)) {
+    data = parse_decl(data);
+  }
+  return data;
+}
+
+
+parse_data *parse_stmt_list(parse_data *data) {
+  while (!bad_exit(data) &&
+         (in_first_set(TOKEN_ID, data) ||
+          in_first_set(TOKEN_SEMI, data) ||
+          in_first_set(TOKEN_KWIF, data) ||
+          in_first_set(TOKEN_KWWHILE, data) ||
+          in_first_set(TOKEN_LBRACE, data)
+          /* || in_first_set(TOKEN_KWRETURN, data) */ )) {
+    data = parse_stmt(data);
+  }
+  return data;
+}
+
+parse_data *parse_stmt(parse_data *data) {
+    if (in_first_set(TOKEN_ID, data)) {
+        update_data_to_next_token(data);
+        data = parse_stmt_after_id(data);
+    }
+    else if (in_first_set(TOKEN_KWIF, data)) {
+        data = parse_if_stmt(data);
+    }
+    else if (in_first_set(TOKEN_KWWHILE, data)) {
+        data = parse_while_stmt(data);
+    }
+    // else if (in_first_set(TOKEN_KWRETURN, data)) { /* TODO */ }
+    else if (in_first_set(TOKEN_LBRACE, data)) {
+        update_data_to_next_token(data);
+        data = parse_opt_var_decls(data); // Call implemented function
+        if (bad_exit(data)) { return data; }
+        data = parse_stmt_list(data);
+        if (bad_exit(data)) { return data; }
+        if (does_not_match(TOKEN_RBRACE, data->current_token)) {
+            data->exit_status = 1; return data;
+        }
+        update_data_to_next_token(data);
+    }
+     else if (in_first_set(TOKEN_SEMI, data)) {
+         update_data_to_next_token(data);
+    }
+     else {
+         data->exit_status = 1;
+     }
+    return data;
+}
+
+parse_data *parse_stmt_after_id(parse_data *data) {
+    if (in_first_set(TOKEN_OPASSG, data)) {
+         update_data_to_next_token(data);
+        data = parse_arith_exp(data);
+        if (bad_exit(data)) { return data; }
+        if (does_not_match(TOKEN_SEMI, data->current_token)) {
+            data->exit_status = 1; return data;
+        }
+        update_data_to_next_token(data);
+    } else if (in_first_set(TOKEN_LPAREN, data)) {
+         update_data_to_next_token(data);
+        data = parse_opt_actuals(data);
+        if (bad_exit(data)) { return data; }
+        if (does_not_match(TOKEN_RPAREN, data->current_token)) {
+            data->exit_status = 1; return data;
+        }
+        update_data_to_next_token(data);
+        if (does_not_match(TOKEN_SEMI, data->current_token)) {
+            data->exit_status = 1; return data;
+        }
+        update_data_to_next_token(data);
+    } else {
+         data->exit_status = 1;
+    }
+    return data;
+}
+
+parse_data *parse_if_stmt(parse_data *data) {
+     update_data_to_next_token(data);
+    if (does_not_match(TOKEN_LPAREN, data->current_token)) {
+         data->exit_status = 1; return data;
+     }
+     update_data_to_next_token(data);
+    data = parse_arith_exp(data);
+    if (bad_exit(data)) { return data; }
+    if (does_not_match(TOKEN_RPAREN, data->current_token)) {
+         data->exit_status = 1; return data;
+     }
+     update_data_to_next_token(data);
+    data = parse_stmt(data);
+    if (bad_exit(data)) { return data; }
+     data = parse_else_part(data);
+     return data;
+}
+
+parse_data *parse_else_part(parse_data *data) {
+     if (in_first_set(TOKEN_KWELSE, data)) {
+         update_data_to_next_token(data);
+         data = parse_stmt(data);
+         if (bad_exit(data)) { return data; }
+     }
+     return data;
+ }
+
+parse_data *parse_while_stmt(parse_data *data) {
+    update_data_to_next_token(data);
+    if (does_not_match(TOKEN_LPAREN, data->current_token)) {
+        data->exit_status = 1; return data;
+    }
+    update_data_to_next_token(data);
+    data = parse_arith_exp(data);
+    if (bad_exit(data)) { return data; }
+    if (does_not_match(TOKEN_RPAREN, data->current_token)) {
+        data->exit_status = 1; return data;
+    }
+    update_data_to_next_token(data);
+    data = parse_stmt(data);
+    return data;
+}
+
+
+parse_data *parse_opt_actuals(parse_data *data) {
+  return data;
+}
+
+
+parse_data *parse_primary_exp(parse_data *data) {
+     if (in_first_set(TOKEN_INTCON, data)) {
+         update_data_to_next_token(data);
+     } else if (in_first_set(TOKEN_ID, data)) {
+         update_data_to_next_token(data);
+     }
+     else {
+         data->exit_status = 1;
+     }
+     return data;
+ }
+
+parse_data *parse_factor(parse_data *data) {
+    data = parse_primary_exp(data);
+    return data;
+}
+
+parse_data *parse_term_prime(parse_data *data) {
+     return data;
+ }
+
+ parse_data *parse_term(parse_data *data) {
+     data = parse_factor(data);
+     if (bad_exit(data)) { return data; }
+     data = parse_term_prime(data);
+     return data;
+ }
+
+ parse_data *parse_arith_exp_prime(parse_data *data) {
+     int is_valid_op_for_condition =
+        in_first_set(TOKEN_OPLE, data) ||
+        in_first_set(TOKEN_OPEQ, data) ||
+        in_first_set(TOKEN_OPNE, data) ||
+        in_first_set(TOKEN_OPLT, data) ||
+        in_first_set(TOKEN_OPGE, data) ||
+        in_first_set(TOKEN_OPGT, data) ||
+        in_first_set(TOKEN_OPASSG, data);
+
+     if (is_valid_op_for_condition) {
+         update_data_to_next_token(data);
+         data = parse_term(data);
+         if (bad_exit(data)) { return data; }
+     }
+     return data;
+ }
+
+ parse_data *parse_arith_exp(parse_data *data) {
+     data = parse_term(data);
+     if (bad_exit(data)) { return data; }
+     data = parse_arith_exp_prime(data);
+     return data;
+ }
+
+
+int parse_prog() {
+  parse_data *data = create_parse_data();
+  data = parse_decl_list(data);
+  const int exit_status = data->exit_status;
+  free(data);
+  return exit_status;
+}
+
+int parse(void) {
+    return parse_prog();
+}
--- assignments/assignment-4-1/tests/test-complete-parser.c
+++ assignments/assignment-4-1/tests/test-complete-parser.c
@@ -65,17 +65,276 @@ static void test_chained_global_variable(void **state) {
   assert_int_equal(1, parser_exit_result);
 }
 
-// static void test_function_definition(void **state) {
-//   (void)state;
-//
-//   const char *basic_ok_func = "int f() { }";
-//   int parser_exit_result = test_parser(basic_ok_func);
-//   assert_int_equal(0, parser_exit_result);
-//
-//   const char *basic_err_func = "int f) { }";
-//   parser_exit_result = test_parser(basic_err_func);
-//   assert_int_equal(1, parser_exit_result);
-// }
+static void test_function_definition(void **state) {
+  (void)state;
+
+  const char *basic_valid = "int f() { }";
+  int parser_exit_result = test_parser(basic_valid);
+  assert_int_equal(0, parser_exit_result);
+
+  const char *different_id = "int main() { }";
+  parser_exit_result = test_parser(different_id);
+  assert_int_equal(0, parser_exit_result);
+
+  const char *missing_type = "f() { }";
+  parser_exit_result = test_parser(missing_type);
+  assert_int_equal(0, parser_exit_result);
+
+  const char *missing_id = "int () { }";
+  parser_exit_result = test_parser(missing_id);
+  assert_int_equal(1, parser_exit_result);
+
+  const char *missing_l_paren = "int f) { }";
+  parser_exit_result = test_parser(missing_l_paren);
+  assert_int_equal(1, parser_exit_result);
+
+  const char *missing_r_paren = "int f( { }";
+  parser_exit_result = test_parser(missing_r_paren);
+  assert_int_equal(1, parser_exit_result);
+
+  const char *missing_l_brace = "int f()  }";
+  parser_exit_result = test_parser(missing_l_brace);
+  assert_int_equal(1, parser_exit_result);
+
+  const char *missing_r_brace = "int f() { ";
+  parser_exit_result = test_parser(missing_r_brace);
+  assert_int_equal(1, parser_exit_result);
+}
+
+static void test_function_call(void **state) {
+  (void) state;
+
+  const char *basic_valid = "int f();";
+  int parser_exit_result = test_parser(basic_valid);
+  assert_int_equal(0, parser_exit_result);
+
+  const char *missing_id = "int ();";
+  parser_exit_result = test_parser(missing_id);
+  assert_int_equal(1, parser_exit_result);
+
+  const char *missing_semi = "int f()";
+  parser_exit_result = test_parser(missing_semi);
+  assert_int_equal(1, parser_exit_result);
+
+  const char *different_id = "int main();";
+  parser_exit_result = test_parser(different_id);
+  assert_int_equal(0, parser_exit_result);
+}
+
+static void test_formals(void **state) {
+  (void) state;
+
+  const char *basic_valid = "int f(int x) { }";
+  int parser_exit_result = test_parser(basic_valid);
+  assert_int_equal(0, parser_exit_result);
+
+  const char *chained_formals = "int f(int x, int y, int z) { }";
+  parser_exit_result = test_parser(chained_formals);
+  assert_int_equal(0, parser_exit_result);
+
+  const char *function_call_formals = "int f(int x, int y, int z);";
+  parser_exit_result = test_parser(function_call_formals);
+  assert_int_equal(0, parser_exit_result);
+
+  const char *missing_id = "int f(int ) { }";
+  parser_exit_result = test_parser(missing_id);
+  assert_int_equal(1, parser_exit_result);
+
+  const char *missing_commas = "int f(int x int y int z) { }";
+  parser_exit_result = test_parser(missing_commas);
+  assert_int_equal(1, parser_exit_result);
+}
+
+static void test_assignment(void **state) {
+  (void) state;
+
+  const char *basic_valid = "int f() { x = 1; }";
+  int parser_exit_result = test_parser(basic_valid);
+  assert_int_equal(0, parser_exit_result);
+
+  const char *different_id = "int f() { y = 1; }";
+  parser_exit_result = test_parser(different_id);
+  assert_int_equal(0, parser_exit_result);
+
+  const char *different_const = "int f() { y = 3; }";
+  parser_exit_result = test_parser(different_const);
+  assert_int_equal(0, parser_exit_result);
+
+  const char *missing_id = "int f() {  = 1; }";
+  parser_exit_result = test_parser(missing_id);
+  assert_int_equal(1, parser_exit_result);
+
+  const char *missing_semi = "int f() { y = 1 }";
+  parser_exit_result = test_parser(missing_semi);
+  assert_int_equal(1, parser_exit_result);
+}
+
+static void test_if_statement(void **state) {
+  (void) state;
+
+  const char *less_equal = "int f() { if (y <= x) { } }";
+  int parser_exit_result = test_parser(less_equal);
+  assert_int_equal(0, parser_exit_result);
+
+  const char *less_than = "int f() { if (y < x) { } }";
+  parser_exit_result = test_parser(less_than);
+  assert_int_equal(0, parser_exit_result);
+
+  const char *greater_than = "int f() { if (y > x) { } }";
+  parser_exit_result = test_parser(greater_than);
+  assert_int_equal(0, parser_exit_result);
+
+  const char *greater_equal = "int f() { if (y >= x) { } }";
+  parser_exit_result = test_parser(greater_equal);
+  assert_int_equal(0, parser_exit_result);
+
+  const char *equal = "int f() { if (y == x) { } }";
+  parser_exit_result = test_parser(equal);
+  assert_int_equal(0, parser_exit_result);
+
+  const char *not_equal = "int f() { if (x != z) { } }";
+  parser_exit_result = test_parser(not_equal);
+  assert_int_equal(0, parser_exit_result);
+
+  const char *missing_parens = "int f() { if x == z { } }";
+  parser_exit_result = test_parser(missing_parens);
+  assert_int_equal(1, parser_exit_result);
+
+  const char *invalid_operator = "int f() { if (x = z) { } }";
+  parser_exit_result = test_parser(invalid_operator);
+  assert_int_equal(0, parser_exit_result);
+
+  const char *nested_if_statements = "int f() { if (x == z) { if (z != y) { } } }";
+  parser_exit_result = test_parser(nested_if_statements);
+  assert_int_equal(0, parser_exit_result);
+}
+
+static void test_while_statement(void **state) {
+  (void) state;
+
+  const char *valid = "int f() { while (z > y) { } }";
+  int parser_exit_result = test_parser(valid);
+  assert_int_equal(0, parser_exit_result);
+
+  const char *bad_operator = "int f() { while (y === z) { } }";
+  parser_exit_result = test_parser(bad_operator);
+  assert_int_equal(1, parser_exit_result);
+}
+
+static void test_else_statement(void **state) {
+  (void) state;
+
+  const char *valid = "int f() { if (x > y) { } else { } }";
+  int parser_exit_result = test_parser(valid);
+  assert_int_equal(0, parser_exit_result);
+}
+
+static void test_local_variable(void **state) {
+  (void) state;
+
+  const char *valid = "int f() { int x; int y; int a, int b, int z; }";
+  int parser_exit_result = test_parser(valid);
+  assert_int_equal(0, parser_exit_result);
+
+  const char *inside_if = "int f() { if (x > y) { int x; int y; } }";
+  parser_exit_result = test_parser(inside_if);
+  assert_int_equal(0, parser_exit_result);
+
+  const char *inside_else = "int f() { if (x > y) { } else { int x; }}";
+  parser_exit_result = test_parser(inside_else);
+  assert_int_equal(0, parser_exit_result);
+
+  const char *inside_while = "int f() { while (x > y) { int x; } }";
+  parser_exit_result = test_parser(inside_while);
+  assert_int_equal(0, parser_exit_result);
+}
+
+static void test_arithmetic_expressions(void **state) {
+  (void) state;
+  int parser_exit_result;
+
+  // --- Basic Binary Operations ---
+  const char *add = "int f() { x = a + 1; }";
+  parser_exit_result = test_parser(add);
+  assert_int_equal(0, parser_exit_result); // Expect success
+
+  const char *sub = "int f() { x = a - b; }";
+  parser_exit_result = test_parser(sub);
+  assert_int_equal(0, parser_exit_result); // Expect success
+
+  const char *mul = "int f() { x = 2 * c; }";
+  parser_exit_result = test_parser(mul);
+  assert_int_equal(0, parser_exit_result); // Expect success
+
+  const char *div = "int f() { x = d / 3; }";
+  parser_exit_result = test_parser(div);
+  assert_int_equal(0, parser_exit_result); // Expect success
+
+  // --- Precedence (* / before + -) ---
+  const char *prec1 = "int f() { x = a + b * c; }"; // Should parse as a + (b * c)
+  parser_exit_result = test_parser(prec1);
+  assert_int_equal(0, parser_exit_result); // Expect success
+
+  const char *prec2 = "int f() { x = a * b - c; }"; // Should parse as (a * b) - c
+  parser_exit_result = test_parser(prec2);
+  assert_int_equal(0, parser_exit_result); // Expect success
+
+  const char *prec3 = "int f() { x = a / b + c * d; }"; // Should parse as (a / b) + (c * d)
+  parser_exit_result = test_parser(prec3);
+  assert_int_equal(0, parser_exit_result); // Expect success
+
+  // --- Associativity (Left-to-right for +, -, *, /) ---
+  const char *assoc1 = "int f() { x = a - b + c; }"; // Should parse as (a - b) + c
+  parser_exit_result = test_parser(assoc1);
+  assert_int_equal(0, parser_exit_result); // Expect success
+
+  const char *assoc2 = "int f() { x = a / b * c; }"; // Should parse as (a / b) * c
+  parser_exit_result = test_parser(assoc2);
+  assert_int_equal(0, parser_exit_result); // Expect success
+
+  // --- Parentheses ---
+  const char *paren1 = "int f() { x = (a + b) * c; }";
+  parser_exit_result = test_parser(paren1);
+  assert_int_equal(0, parser_exit_result); // Expect success
+
+  const char *paren2 = "int f() { x = a * (b - c); }";
+  parser_exit_result = test_parser(paren2);
+  assert_int_equal(0, parser_exit_result); // Expect success
+
+  const char *paren3 = "int f() { x = a / (b + c * d); }";
+  parser_exit_result = test_parser(paren3);
+  assert_int_equal(0, parser_exit_result); // Expect success
+
+  // --- Unary Minus (Needs Factor rule update) ---
+  // const char *unary1 = "int f() { x = -a; }";
+  // parser_exit_result = test_parser(unary1);
+  // assert_int_equal(0, parser_exit_result); // Expect success (Will fail until factor rule is updated)
+
+  // const char *unary2 = "int f() { x = b * -c; }"; // Needs precedence handling for unary vs binary
+  // parser_exit_result = test_parser(unary2);
+  // assert_int_equal(0, parser_exit_result); // Expect success (Will fail until factor rule is updated)
+
+  // --- Error Cases ---
+  const char *err1 = "int f() { x = a + ; }"; // Missing operand
+  parser_exit_result = test_parser(err1);
+  assert_int_equal(1, parser_exit_result); // Expect failure
+
+  const char *err2 = "int f() { x = * b; }"; // Missing operand
+  parser_exit_result = test_parser(err2);
+  assert_int_equal(1, parser_exit_result); // Expect failure
+
+  const char *err3 = "int f() { x = a + * b; }"; // Adjacent operators
+  parser_exit_result = test_parser(err3);
+  assert_int_equal(1, parser_exit_result); // Expect failure
+
+  const char *err4 = "int f() { x = (a + b; }"; // Missing closing parenthesis
+  parser_exit_result = test_parser(err4);
+  assert_int_equal(1, parser_exit_result); // Expect failure
+
+  const char *err5 = "int f() { x = a (+) b; }"; // Invalid operator placement
+  parser_exit_result = test_parser(err5);
+  assert_int_equal(1, parser_exit_result); // Expect failure
+}
 
 bool DEBUG_ON = false;
 
@@ -83,6 +342,15 @@ int main(void) {
   const struct CMUnitTest tests[] = {
     cmocka_unit_test(test_global_variable),
     cmocka_unit_test(test_chained_global_variable),
+    cmocka_unit_test(test_function_definition),
+    cmocka_unit_test(test_function_call),
+    cmocka_unit_test(test_formals),
+    cmocka_unit_test(test_assignment),
+    cmocka_unit_test(test_if_statement),
+    cmocka_unit_test(test_while_statement),
+    cmocka_unit_test(test_else_statement),
+    cmocka_unit_test(test_local_variable),
+    cmocka_unit_test(test_arithmetic_expressions),
   };
 
   return cmocka_run_group_tests(tests, NULL, NULL);
