b032112fe43b467925291dbf3dc85cfcc8db4120
Matthew Song <matthewsong18@gmail.com>
2025-04-28 01:02:22 -0700

    test: allocate symbol table

--- assignments/assignment-4-1/tests/test-semantic.c
+++ assignments/assignment-4-1/tests/test-semantic.c
@@ -17,9 +17,21 @@ static void test_allocate_symbol(void **state) {
   assert_int_equal(SYM_NULL, symbol->type);
 }
 
+static void test_allocate_symbol_table(void **state) {
+  (void) state;
+
+  SymbolTable *symbol_table = allocate_symbol_table();
+
+  assert_non_null(symbol_table);
+  assert_null(symbol_table->symbols);
+  assert_null(symbol_table->global_scope);
+  assert_null(symbol_table->current_scope);
+}
+
 int main(void) {
   const struct CMUnitTest tests[] = {
     cmocka_unit_test(test_allocate_symbol),
+    cmocka_unit_test(test_allocate_symbol_table),
   };
 
   return cmocka_run_group_tests(tests, NULL, NULL);
