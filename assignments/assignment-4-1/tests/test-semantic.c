d4058f64bb4cab3af0d529bb55e2ba6a07fe6dd9
Matthew Song <matthewsong18@gmail.com>
2025-04-28 00:31:42 -0700

    test: add global symbols to symbol table

--- assignments/assignment-4-1/tests/test-semantic.c
+++ assignments/assignment-4-1/tests/test-semantic.c
@@ -6,13 +6,20 @@
 #include <cmocka.h>
 
 // Headers
+#include "../src/features/semantic/symbols.h"
 
+static void test_allocate_symbol(void **state) {
+  (void) state;
 
-
+  Symbol *symbol = allocate_symbol();
+  assert_non_null(symbol);
+  assert_null(symbol->name);
+  assert_int_equal(SYM_NULL, symbol->type);
+}
 
 int main(void) {
   const struct CMUnitTest tests[] = {
-
+    cmocka_unit_test(test_allocate_symbol),
   };
 
   return cmocka_run_group_tests(tests, NULL, NULL);
