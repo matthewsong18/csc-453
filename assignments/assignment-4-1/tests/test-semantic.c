9e6b028f12d71868b6b005f7efbc20b8109b093f
Matthew Song <matthewsong18@gmail.com>
2025-04-27 23:58:31 -0700

    build: add test_semantic

---
+++ assignments/assignment-4-1/tests/test-semantic.c
@@ -0,0 +1,19 @@
+// Cmocka
+#include <setjmp.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <cmocka.h>
+
+// Headers
+
+
+
+
+int main(void) {
+  const struct CMUnitTest tests[] = {
+
+  };
+
+  return cmocka_run_group_tests(tests, NULL, NULL);
+}
